5b65b41d2022d4f6878cb80aedd271e686dc1535
Kedssy <90483207+Kedssy@users.noreply.github.com>
2022-11-19 10:30:40 -0300

    Commit inicial do projeto

---
+++ FlashCar/flashcar-main/src/main/java/br/com/unipar/flashcar/exception/DescricaoInvalidaException.java
@@ -0,0 +1,8 @@
+package br.com.unipar.flashcar.exception;
+
+public class DescricaoInvalidaException extends Exception {
+
+    public DescricaoInvalidaException() {
+        super("Descrição Vazia ou invalida. Verifique!");
+    }
+}
